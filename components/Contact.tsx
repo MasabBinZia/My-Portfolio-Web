c2409cf9c495c5f97c4c7badb264b2505e2e3193
Masab Bin Zia <masabmbz5@gmail.com>
2023-05-05 13:58:43 -0700

    ❤️ intial Commit

---
+++ components/Contact.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image';
+import { contact } from '@/data/config';
+
+export default function Stack() {
+  return (
+    <div>
+      <div className="overflow-x-hidden w-full">
+        <h2 className="landingSectionTitle max-w-max mx-0 text-left relative mb-4 md:w-max ">
+          {contact.title}
+        </h2>
+      </div>
+      <p>
+      Reach out by email:{' '}
+        <a
+          className="dark:text-white text-black transition-colors duration-500"
+          href={`mailto:${contact.email}`}
+        >
+          {contact.email}
+        </a>
+      </p>
+      <div className="flex space-x-5 mt-5 text-lightText transition-colors duration-500">
+        {contact.github && (
+          <a
+            href={`https://github.com/${contact.github}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/static/icons/github.svg"
+              width={20}
+              height={20}
+              alt="Github icon"
+            />
+          </a>
+        )}
+        {contact.linkedin && (
+          <a
+            href={`https://linkedin.com/in/${contact.linkedin}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/static/icons/linkedin.svg"
+              width={20}
+              height={20}
+              alt="LinkedIn icon"
+            />
+          </a>
+        )}
+        {contact.linkedin && (
+          <a
+            href={`https://twitter.com/${contact.twitter}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/static/icons/twitter.svg"
+              width={20}
+              height={20}
+              alt="twitter icon"
+            />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ data/config.tsx
@@ -0,0 +1,61 @@
+export const meta = {
+    title: 'Masab Bin Zia - FrontEnd Dev',
+    description: "Hey, I\'m Masab Bin Zia. I\'m a web developer bringing people's ideas to life."
+  }
+  
+  export const hero = {
+    title: "👋 Hey there! I'm Masab Bin Zia.",
+    desc: "💫 How about I create engaging and intuitive digital experiences as a Junior Frontend Developer, bringing people's ideas to life?",
+    more:"🌱 I’m currently learning Typescript,Next.js,Web3.0 and Back-End Development"
+  };
+  
+  export const projects = {
+    title: 'My Projects',
+    desc: "Explore some of my proudest accomplishments in the form of the projects I've brought to life.",
+    projects: [
+      {
+        title: 'TokenExchangePro',
+        description: 'Simple Crypto Trading Landing Page.',
+        link: 'https://tokenexchangepro.netlify.app/',
+        github: 'https://github.com/MasabBinZia/TokenExchangePro'
+      },
+      {
+        title: 'AliAhmed_Portfolio',
+        description: 'Minimal React Porfolio!',
+        link: 'http://hateful-floor.surge.sh',
+        github: 'https://github.com/MasabBinZia/AliAhmed_Portfolio'
+      },
+      {
+        title: 'Sci-Fi City ',
+        description: 'Sci city react parallax-app for paractise!',
+        link: 'http://sci-fi-city.surge.sh/',
+        github: 'https://github.com/MasabBinZia/Simple-ReactParallax-app'
+      }
+    ]
+  };
+  
+  export const stack = {
+    title: 'My Tech Stack',
+    stack: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Typescript',
+      'React',
+      'NextJS',
+      'TailwindCSS',
+      'Git',
+      '.Net',
+      'Nodejs',
+      'Bootstrap'
+    ]
+  };
+  
+  export const contact = {
+    title: 'Contact Me',
+    email: 'masabmbz5@gmail.com',
+    github: 'MasabBinZia',
+    linkedin: 'masab-bin-zia-411a0922a',
+    twitter: ''
+  };
+  
