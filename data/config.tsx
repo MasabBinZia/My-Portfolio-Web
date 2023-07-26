eb159ef009e52f31ae91f641d931020480bdda82
Masab Bin Zia <masabmbz5@gmail.com>
2023-07-26 12:27:13 +0500

    updated-pfolio

--- data/config.tsx
+++ data/config.tsx
@@ -1,61 +1,62 @@
 export const meta = {
-    title: 'Masab Bin Zia - FrontEnd Dev',
-    description: "Hey, I\'m Masab Bin Zia. I\'m a web developer bringing people's ideas to life."
-  }
-  
-  export const hero = {
-    title: "👋 Hey there! I'm Masab Bin Zia.",
-    desc: "💫 How about I create engaging and intuitive digital experiences as a Junior Frontend Developer, bringing people's ideas to life?",
-    more:"🌱 I’m currently learning Typescript,Next.js,Web3.0 and Back-End Development"
-  };
-  
-  export const projects = {
-    title: 'My Projects',
-    desc: "Explore some of my proudest accomplishments in the form of the projects I've brought to life.",
-    projects: [
-      {
-        title: 'TokenExchangePro',
-        description: 'Simple Crypto Trading Landing Page.',
-        link: 'https://tokenexchangepro.netlify.app',
-        github: 'MasabBinZia/TokenExchangePro'
-      },
-      {
-        title: 'AliAhmed_Portfolio',
-        description: 'Minimal React Porfolio!',
-        link: 'http://hateful-floor.surge.sh',
-        github: 'MasabBinZia/AliAhmed_Portfolio'
-      },
-      {
-        title: 'Sci-Fi City ',
-        description: 'Sci city react parallax-app for paractise!',
-        link: 'http://sci-fi-city.surge.sh',
-        github: 'MasabBinZia/Simple-ReactParallax-app'
-      }
-    ]
-  };
-  
-  export const stack = {
-    title: 'My Tech Stack',
-    stack: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'Typescript',
-      'React',
-      'NextJS',
-      'TailwindCSS',
-      'Git',
-      '.Net',
-      'Nodejs',
-      'Bootstrap'
-    ]
-  };
-  
-  export const contact = {
-    title: 'Contact Me',
-    email: 'masabmbz5@gmail.com',
-    github: 'MasabBinZia',
-    linkedin: 'masab-bin-zia-411a0922a',
-    twitter: 'hadescreeping'
-  };
-  
+  title: "Masab Bin Zia - FrontEnd Dev",
+  description:
+    "Hey, I'm Masab Bin Zia. I'm a web developer bringing people's ideas to life.",
+};
+
+export const hero = {
+  title: "👋 Hey there! I'm Masab Bin Zia.",
+  desc: "💫 How about I create engaging and intuitive digital experiences as a Junior Frontend Developer, bringing people's ideas to life?",
+  more: "🌱 I’m currently learning Typescript,Next.js,Web3.0 and Back-End Development",
+};
+
+export const projects = {
+  title: "My Projects",
+  desc: "Explore some of my proudest accomplishments in the form of the projects I've brought to life.",
+  projects: [
+    {
+      title: "Dine Market",
+      description:
+        "Dine Market is an e-commerce project developed for the PIAIC hackathon.",
+      link: "https://dine-market-ecommerce-web.vercel.app",
+      github: "MasabBinZia/Hackthon-01-web-Ecommerce-web",
+    },
+    {
+      title: "AliAhmed_Portfolio",
+      description: "Minimal React Porfolio!",
+      link: "http://hateful-floor.surge.sh",
+      github: "MasabBinZia/AliAhmed_Portfolio",
+    },
+    {
+      title: "NFT Marketplace",
+      description: " 🚀 Welcome to our cutting-edge NFT Marketplace. ",
+      link: "https://nft-market-place-one-taupe.vercel.app",
+      github: "MasabBinZia/NFT-MarketPlace",
+    },
+  ],
+};
+
+export const stack = {
+  title: "My Tech Stack",
+  stack: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Typescript",
+    "React",
+    "NextJS",
+    "TailwindCSS",
+    "Git",
+    ".Net",
+    "Nodejs",
+    "Bootstrap",
+  ],
+};
+
+export const contact = {
+  title: "Contact Me",
+  email: "masabmbz5@gmail.com",
+  github: "MasabBinZia",
+  linkedin: "masab-bin-zia-411a0922a",
+  twitter: "hadescreeping",
+};
